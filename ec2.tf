ccc1da137855561e4dacd4d017d4b951be4291c6
Praveen <saipraveenneelisetty27@gmail.com>
2024-07-09 21:42:37 +0530

    Setting up Ec2 instance with terraform

---
+++ ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "aws_security" {
+
+name = "allow ssh"
+description = "Allowing SSH port"
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }  
+
+    tags = {
+        Name = "allow ssh"
+        CreatedBy = "Praveen N"
+    }
+}
+
+resource "aws_instance" "db" {
+    ami = "ami-090252cbe067a9e58"
+    vpc_security_group_ids = [aws_security_group.aws_security.id] 
+    instance_type = "t2.micro"  
+
+    tags =  {
+        Name = "db"
+        CreatedBy = "Praveen"
+    }
+}
