3c2dd3339e2df391587a8502159567ca700b73da
Praveen <saipraveenneelisetty27@gmail.com>
2024-07-13 23:14:32 +0530

    Setting up Backend by using Ansible

--- dynamic/provider.tf
+++ dynamic/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.58.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "praveen-remote-state-s3"
+    key            = "dynamic-demo"
+    region         = "us-east-1"
+    dynamodb_table = "dynamic-locking"
+  }
 }
 
 provider "aws" {
